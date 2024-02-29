12854896f685d978cbbb4f711e053316954e0513
manojkoyilada <koyiladakarthik55@gmail.com>
2024-02-29 19:52:30 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import logo from "./assets/investment-calculator-logo.png";
+import Header from "./Components/Header/Header";
+import UserInput from "./Components/UserInput/UserInput";
+import ResultsTable from "./Components/ResultsTable/ResultsTable";
+import { useState } from "react";
+
+function App() {
+  const [userInput, setUserInput] = useState(null);
+
+  const calculateHandler = (userInput) => {
+    setUserInput(userInput);
+  };
+  const yearlyData = []; // per-year results
+
+  if (userInput) {
+    let currentSavings = +userInput["current-savings"]; // feel free to change the shape of this input object!
+    const yearlyContribution = +userInput["yearly-contribution"]; // as mentioned: feel free to change the shape...
+    const expectedReturn = +userInput["expected-return"] / 100;
+    const duration = +userInput["duration"];
+
+    // The below code calculates yearly results (total savings, interest etc)
+    for (let i = 0; i < duration; i++) {
+      const yearlyInterest = currentSavings * expectedReturn;
+      currentSavings += yearlyInterest + yearlyContribution;
+      yearlyData.push({
+        // feel free to change the shape of the data pushed to the array!
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        yearlyContribution: yearlyContribution,
+      });
+    }
+  }
+
+  return (
+    <div>
+      <Header />
+      <UserInput onCalculate={calculateHandler} />
+      {!userInput && (
+        <center>
+          <p>No investment calculated yet!</p>
+        </center>
+      )}
+      {userInput && (
+        <ResultsTable
+          data={yearlyData}
+          initialInvestment={userInput["current-savings"]}
+        />
+      )}
+      {/* Todo: Show below table conditionally (only once result data is available) */}
+      {/* Show fallback text if no data is available */}
+    </div>
+  );
+}
+
+export default App;
